7307e13ece4db62c5c0c695edd67da2a58433186
kkulesz <kulesza.konrad@protonmail.com>
2022-06-10 19:02:37 +0200

    starts working

--- exc1.R
+++ exc1.R
@@ -1,7 +1,14 @@
+# set params of experiment
 set.seed(2137)
 our_lambda <- 3/2
 use_analitical_solution <- TRUE
+start <- 10
+step <- 1
+no_of_steps <- 1000
+no_of_simulations_in_single_step <- 100
 
+
+# do experiment
 log_like_fn <- function(lambda, X){
   X <- as.matrix(X)
   n <- length(X)
@@ -11,29 +18,35 @@ log_like_fn <- function(lambda, X){
 }
 
 simulate_for_sample_size <- function(lambda, sample_size){
-  random_sample <- rexp(sample_size, lambda)
   
-  # 1. calculate maximum likelihood estimation
-  if (use_analitical_solution){
-    # https://www.statlect.com/fundamentals-of-statistics/exponential-distribution-maximum-likelihood
-    est = length(random_sample)/sum(random_sample)
-  }else{
-    # https://www.youtube.com/watch?v=w3drLH-DFpE
-    mle_est <- optim(par=3, fn=log_like_fn, lower=0, upper=Inf, method="L-BFGS-B", X = random_sample)
-    est <- mle_est$par
+  estimators = c()
+  for (i in 1:no_of_simulations_in_single_step){
+    random_sample <- rexp(sample_size, lambda)
+    
+    # 1. calculate maximum likelihood estimations
+    if (use_analitical_solution){
+      # https://www.statlect.com/fundamentals-of-statistics/exponential-distribution-maximum-likelihood
+      est = length(random_sample)/sum(random_sample)
+    }else{
+      # https://www.youtube.com/watch?v=w3drLH-DFpE
+      mle_est <- optim(par=3, fn=log_like_fn, lower=0, upper=Inf, method="L-BFGS-B", X = random_sample)
+      est <- mle_est$par
+    }
+    
+    estimators <- c(estimators, est)
   }
+  final_est <- mean(estimators)
   
   # 2. calculate its bias
-  bias <- est - lambda # abs?
+  bias <- final_est - lambda # abs?
   
   # 3. calculate cr bound
-  numerator <- 1 + 1/(sample_size-1)
-  fisher_inf <- 1/(lambda^2) #https://www-users.mat.umk.pl/~alzaig/inf_Fi.pdf
-  # https://www-users.mat.umk.pl/~alzaig/nie_C_R.pdf
-  cr_bound <- numerator / (sample_size * fisher_inf)
+  fisher_inf <- 1/(lambda^2) 
+  m_prim <- 1
+  cr_bound <- m_prim / (sample_size * fisher_inf)
   
   # 4. calculate variance
-  variance <- mean((random_sample - lambda)^2) #???????
+  variance <- mean((estimators - lambda)^2)
   
   return (c(sample_size, est, bias, variance, cr_bound))
 }
@@ -45,8 +58,9 @@ column_names <- c('sample_size', 'estimated_lambda', 'bias', 'variance', 'cr_bou
 results <- data.frame(matrix(ncol = length(column_names), nrow = 0))
 colnames(results) <- column_names
 
-for (i in 1:100){
-  sample_size <- 1000 * i
+for (i in 1:no_of_steps){
+  sample_size <- start + step * (i-1)
+  print(sample_size)
   result <- simulate_for_sample_size(our_lambda, sample_size)
   
   result <- as.data.frame(t(result))
@@ -56,12 +70,17 @@ for (i in 1:100){
 print(results)
 
 
-# plot results somehow
+# plot bias
 plot(results$bias ~ results$sample_size, 
      main = 'Obciążenie od liczności próby', xlab = 'liczność próby', ylab = 'obciążenie')
+abline(h = 0, col = 'red', lty=2)
 
-plot(results$variance ~ results$sample_size, 
-     main = 'Wariancja od liczności próby', xlab = 'liczność próby', ylab = 'wariancja')
-
+# plot cr-bound and variance
 plot(results$cr_bound ~ results$sample_size, 
-     main = 'Kres CR od liczności próby', xlab = 'liczność próby', ylab = 'CR')
+     main = 'siema', xlab = 'liczność próby', ylab = 'wartość', type = 'l', col = 'red', lwd = 2, ylim=c(0,1), log = 'x')
+lines(results$sample_size, results$variance, col='blue', lwd = 1)
+legend(x = 'topright', c('variance', 'cr-bound'), col = c('blue', 'red'), lty=c(1, 1), lwd = 2)
+
+# links to not forget them
+# https://www-users.mat.umk.pl/~alzaig/inf_Fi.pdf
+# https://www-users.mat.umk.pl/~alzaig/nie_C_R.pdf
