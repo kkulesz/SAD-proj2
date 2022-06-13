12a8f4bf77b98fd71fec27cf06ee8c423d3d3690
kkulesz <kulesza.konrad@protonmail.com>
2022-06-13 10:49:31 +0200

    tidy up a little

--- exc1.R
+++ exc1.R
@@ -1,44 +1,25 @@
-# set params of experiment
+#### set params of experiment
 set.seed(2137)
 our_lambda <- 3/2
-use_analitical_solution <- TRUE
 start <- 10
-step <- 1
-no_of_steps <- 1000
-no_of_simulations_in_single_step <- 100
+step <- 10
+no_of_steps <- 100
+no_of_simulations_in_single_step <- 1000
 
-
-# do experiment
-log_like_fn <- function(lambda, X){
-  X <- as.matrix(X)
-  n <- length(X)
-  fn <- n*log(lambda) - lambda*sum(X)
-  
-  return (-fn)
-}
-
+#### help functions
 simulate_for_sample_size <- function(lambda, sample_size){
   
+  # 1. calculate maximum likelihood estimations
   estimators = c()
   for (i in 1:no_of_simulations_in_single_step){
     random_sample <- rexp(sample_size, lambda)
-    
-    # 1. calculate maximum likelihood estimations
-    if (use_analitical_solution){
-      # https://www.statlect.com/fundamentals-of-statistics/exponential-distribution-maximum-likelihood
-      est = length(random_sample)/sum(random_sample)
-    }else{
-      # https://www.youtube.com/watch?v=w3drLH-DFpE
-      mle_est <- optim(par=3, fn=log_like_fn, lower=0, upper=Inf, method="L-BFGS-B", X = random_sample)
-      est <- mle_est$par
-    }
-    
+    est = length(random_sample)/sum(random_sample)
     estimators <- c(estimators, est)
   }
   final_est <- mean(estimators)
   
   # 2. calculate its bias
-  bias <- final_est - lambda # abs?
+  bias <- final_est - lambda
   
   # 3. calculate cr bound
   fisher_inf <- 1/(lambda^2) 
@@ -48,34 +29,31 @@ simulate_for_sample_size <- function(lambda, sample_size){
   # 4. calculate variance
   variance <- mean((estimators - lambda)^2)
   
-  # TODO
-
-  if(sample_size %% 300 == 0){
-    print('jestem ')
+  # 5. plot normalized for chosen sample sizes
+  if(sample_size == 10 || sample_size == 100 || sample_size == 1000 || sample_size == 10000){
     normalized <- sqrt(sample_size) * (estimators - lambda)
-    hist(normalized, prob=TRUE, xlab="max. temp. diff.", main=as.character(sample_size), xlim=c(-10,10), breaks = 20)
+    hist(normalized, prob=TRUE, xlab="Value", main=paste('Normalized for sample size = ', as.character(sample_size)), xlim=c(-6,6), ylim=c(0, 0.5), breaks = 100)
+    norm_sd = sqrt(sample_size) * sqrt(variance)
+    curve(dnorm(x, mean=0, sd=norm_sd), add=TRUE, col = 'red', lwd = 2)
   }
   
   return (c(sample_size, est, bias, variance, cr_bound))
 }
 
 
-################################################################################
-
+#### do experiment
 column_names <- c('sample_size', 'estimated_lambda', 'bias', 'variance', 'cr_bound')
 results <- data.frame(matrix(ncol = length(column_names), nrow = 0))
 colnames(results) <- column_names
 
 for (i in 1:no_of_steps){
   sample_size <- start + step * (i-1)
-  print(sample_size)
   result <- simulate_for_sample_size(our_lambda, sample_size)
   
   result <- as.data.frame(t(result))
   colnames(result) <- column_names
   results <- rbind(results, result)
 }
-print(results)
 
 
 # plot bias
@@ -85,10 +63,6 @@ abline(h = 0, col = 'red', lty=2)
 
 # plot cr-bound and variance
 plot(results$cr_bound ~ results$sample_size, 
-     main = 'siema', xlab = 'liczność próby', ylab = 'wartość', type = 'l', col = 'red', lwd = 2, ylim=c(0,1), log = 'x')
+     main = 'siema', xlab = 'liczność próby', ylab = 'wartość', type = 'l', col = 'red', lwd = 2, ylim=c(0, 0.5), log = 'x')
 lines(results$sample_size, results$variance, col='blue', lwd = 1)
 legend(x = 'topright', c('variance', 'cr-bound'), col = c('blue', 'red'), lty=c(1, 1), lwd = 2)
-
-# links to not forget them
-# https://www-users.mat.umk.pl/~alzaig/inf_Fi.pdf
-# https://www-users.mat.umk.pl/~alzaig/nie_C_R.pdf
